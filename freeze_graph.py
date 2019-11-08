4739935f710fd99ac3af74be5c52169bc3d83100
liheng <liheng@isurro.com>
2019-11-08 17:44:36 +0800

    2019.11.8-2

--- freeze_graph.py
+++ freeze_graph.py
@@ -11,10 +11,11 @@
 __author__ = 'liheng'
 
 import tensorflow as tf
+from tensorflow.python.platform import gfile
 from core.yolov3 import YOLOV3
 
-pb_file = "./yolov3_coco.pb" # 保存的.pb文件路径
-ckpt_file = "./checkpoint/yolov3_coco_demo.ckpt" # 待转换的.ckpt文件路径
+pb_file = "./yolov3_bdd100k.pb" # 保存的.pb文件路径
+ckpt_file = "/home/liheng/liheng/liheng/bdd100k_mobilenet_checkpoint/yolov3_model_18-epoch.ckpt-13500" # 待转换的.ckpt文件路径
 
 # 需要保存的指定的 节点 名称,而非张量名称
 # 节点名称 pred_sbbox 指变量作用空间，concat_2来源于 decode 函数tf.concat操作，由于代码中未显式指定该操作的名称，因此给予了默认名称
@@ -23,15 +24,19 @@ ckpt_file = "./checkpoint/yolov3_coco_demo.ckpt" # 待转换的.ckpt文件路径
 # Tensor("pred_mbbox/concat_2:0", shape=(?, ?, ?, 3, 85), dtype=float32) \
 # Tensor("pred_lbbox/concat_2:0", shape=(?, ?, ?, 3, 85), dtype=float32)
 # 故务必对concat_2的来源有所知晓！
-output_node_names = ["input/input_data", "pred_sbbox/concat_2", "pred_mbbox/concat_2", "pred_lbbox/concat_2"]
+# output_node_names = ["input/input_data", "pred_sbbox/concat_2", "pred_mbbox/concat_2", "pred_lbbox/concat_2"]
+output_node_names = ["input/input_data", "pred_res/pred_bboxes"]# 最终结果
+# output_node_names = ["input/input_data", "pred_sbbox/concat_2", "pred_mbbox/concat_2", "pred_lbbox/concat_2","pred_res/pred_bboxes"]
 
 # 定义模型的输入
 with tf.name_scope('input'):
     input_data = tf.placeholder(dtype=tf.float32, name='input_data')
+    trainable = tf.convert_to_tensor(False,dtype=tf.bool,name='training')
 
-model = YOLOV3(input_data, trainable=False)# 恢复模型之前，首先定义一遍网络结构，然后才能把变量的值恢复到网络中,注意此处trainable=False
+model = YOLOV3(input_data, trainable=trainable)# 恢复模型之前，首先定义一遍网络结构，然后才能把变量的值恢复到网络中,注意此处trainable=False
 print(model.conv_sbbox, model.conv_mbbox, model.conv_lbbox)
 print(model.pred_sbbox, model.pred_mbbox, model.pred_lbbox)
+print(model.pred_res_boxes)
 
 sess  = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
 saver = tf.train.Saver()
@@ -43,5 +48,53 @@ converted_graph_def = tf.graph_util.convert_variables_to_constants(sess,
                                                     # 只有定义了输出结点,freeze才会把得到
                                                     # 输出结点所必要的结点都保存下来,或者哪些结点可以丢弃
 
+print('%d ops in the final graph.' % len(converted_graph_def.node))  # 得到当前图有几个操作节点
+print('The op.name and op.valuse() are below: ')
+for op in sess.graph.get_operations():
+   print(op.name, op.values())
+
+
+print('Fix nodes: ')
+# fix nodes
+for node in converted_graph_def.node:
+    if node.op == 'RefSwitch':
+        node.op = 'Switch'
+        for index in range(len(node.input)):
+            if 'moving_' in node.input[index]:
+                node.input[index] = node.input[index] + '/read'
+    elif node.op == 'AssignSub':
+        node.op = 'Sub'
+        if 'use_locking' in node.attr: del node.attr['use_locking']
+    elif node.op == 'AssignAdd':
+        node.op = 'Add'
+        if 'use_locking' in node.attr: del node.attr['use_locking']
+    elif node.op == 'Assign':
+        node.op = 'Identity'
+        if 'use_locking' in node.attr: del node.attr['use_locking']
+        if 'validate_shape' in node.attr: del node.attr['validate_shape']
+        if len(node.input) == 2:
+            # input0: ref: Should be from a Variable node. May be uninitialized.
+            # input1: value: The value to be assigned to the variable.
+            node.input[0] = node.input[1]
+            del node.input[1]
+    # print(node.op,':',node.attr)
+
+
 with tf.gfile.GFile(pb_file, "wb") as f:
     f.write(converted_graph_def.SerializeToString())#将图定义转化为字符串形式并且写入.pb文件中
+
+
+
+sess = tf.Session()
+with gfile.FastGFile(pb_file, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    sess.graph.as_default()
+    tf.import_graph_def(graph_def, name='')  # 导入计算图
+
+# 需要有一个初始化的过程
+sess.run(tf.global_variables_initializer())
+for op in sess.graph.get_operations():
+   print(op.name, op.values())
+
+exit(0)
--- video_demo_2.py
+++ video_demo_2.py
@@ -19,13 +19,13 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "" #不采用GPU
 
 # 张量名称
-return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0", "pred_mbbox/concat_2:0", "pred_lbbox/concat_2:0"]
+return_elements = ["input/input_data:0", "pred_res/pred_bboxes:0"]
 pb_file         = "./yolov3_bdd100k.pb"
 video_path      = "/home/liheng/ADAS_Video/1105/ADAS_Video-20191105-145101.mp4"
 bSaveResult     = False  # 是否保存结果视频
 # video_path      = 0
 num_classes     = 8 # 检测目标数量
-input_size      = 416
+input_size      = 416 # 320
 graph           = tf.Graph()
 return_tensors  = utils.read_pb_return_tensors(graph, pb_file, return_elements)
 
@@ -40,7 +40,7 @@ with tf.Session(graph=graph) as sess:
 
     nWaitTime = 1
     nFrameIdx = 0
-    #cv2.namedWindow("result", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("result", cv2.WINDOW_NORMAL)
     while True:
         return_value, frame = vid.read()
         nFrameIdx += 1
@@ -51,23 +51,30 @@ with tf.Session(graph=graph) as sess:
 
         frame_size = frame.shape[:2]
         image_data = utils.image_preprocess(np.copy(frame), [input_size, input_size])#图像预处理,转换为RGB格式，并进行缩放
+
+        frame = image_data*255
+        frame = frame.astype(np.uint8)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
         image_data = image_data[np.newaxis, ...]#增加一维，shape从(416,416,3)变为(1,416,416,3)
 
         prev_time = time.time()
 
-        pred_sbbox, pred_mbbox, pred_lbbox = sess.run(
-            [return_tensors[1], return_tensors[2], return_tensors[3]],
-                    feed_dict={ return_tensors[0]: image_data})
+        pred_bbox = sess.run(
+            [return_tensors[1]],
+            feed_dict={ return_tensors[0]: image_data})
+        bboxes = np.reshape(pred_bbox,(-1,6))
         # pred_sbbox.shape (1,52,52,3,85)
         # pred_mbbox.shape (1,26,26,3,85)
         # pred_lbbox.shape (1,13,13,3,85)
 
-        pred_bbox = np.concatenate([np.reshape(pred_sbbox, (-1, 5 + num_classes)),#shape:(8112,85)
-                                    np.reshape(pred_mbbox, (-1, 5 + num_classes)),#shape:(2028,85)
-                                    np.reshape(pred_lbbox, (-1, 5 + num_classes))], axis=0)#shape:(507,85),pred_bbox.shape:(10647,85)
+        # pred_bbox = np.concatenate([np.reshape(pred_sbbox, (-1, 5 + num_classes)),#shape:(8112,85)
+        #                             np.reshape(pred_mbbox, (-1, 5 + num_classes)),#shape:(2028,85)
+        #                             np.reshape(pred_lbbox, (-1, 5 + num_classes))], axis=0)#shape:(507,85),pred_bbox.shape:(10647,85)
 
-        bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, 0.3)#
-        bboxes = utils.nms(bboxes, 0.45, method='nms')
+        # bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, 0.3)#
+        # bboxes = utils.nms(bboxes, 0.45, method='nms')
+
         image = utils.draw_bbox(frame, bboxes)
 
         curr_time = time.time()
@@ -75,7 +82,7 @@ with tf.Session(graph=graph) as sess:
 
         result = np.asarray(image)
         info = "Frame:%d Fps: %.2f time: %.2f ms" %(nFrameIdx,1.0/exec_time,1000*exec_time)
-        cv2.putText(result, info, (0,25), cv2.FONT_HERSHEY_SIMPLEX,1.0, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+        cv2.putText(result, info, (0,25), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 255), 1, lineType=cv2.LINE_AA)
         cv2.imshow("result", result)
         if bSaveResult:
             videoWriter.write(result)
